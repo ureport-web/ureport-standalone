8cce33f19240373c9f2fe78c5e78e46fedea10e8
Ji <yizhong.ji@plusgrade.com>
2026-05-21 19:31:15 -0400

    fix prod login issue

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ if (config !== undefined) {
    * sessions
    */
 
+  app.set('trust proxy', 1);
   app.use(cookieParser());
   // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
   // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
